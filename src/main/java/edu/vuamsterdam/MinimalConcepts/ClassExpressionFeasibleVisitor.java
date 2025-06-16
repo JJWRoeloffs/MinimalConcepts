0419b34284f1e9071ec650dc979fae4d064fa239
JJWRoeloffs <jelleroeloffs@gmail.com>
2025-06-16 11:19:31 +0200

    Write main to run on arg ontology

---
+++ src/main/java/edu/vuamsterdam/MinimalConcepts/ClassExpressionFeasibleVisitor.java
@@ -0,0 +1,94 @@
+package edu.vuamsterdam.MinimalConcepts;
+
+import org.semanticweb.owlapi.model.*;
+
+public class ClassExpressionFeasibleVisitor implements OWLClassExpressionVisitorEx<Boolean> {
+    public ClassExpressionFeasibleVisitor() {
+        super();
+    }
+
+    @Override
+    public Boolean visit(OWLClass ce) {
+        return true;
+    }
+
+    @Override
+    public Boolean visit(OWLObjectIntersectionOf ce) {
+        return ce.operands().anyMatch(op -> op.accept(this));
+    }
+
+    @Override
+    public Boolean visit(OWLObjectUnionOf ce) {
+        return ce.operands().anyMatch(op -> op.accept(this));
+    }
+
+    @Override
+    public Boolean visit(OWLObjectComplementOf ce) {
+        return ce.getOperand().accept(this);
+    }
+
+    @Override
+    public Boolean visit(OWLObjectSomeValuesFrom ce) {
+        return ce.getFiller().accept(this);
+    }
+
+    @Override
+    public Boolean visit(OWLObjectAllValuesFrom ce) {
+        return ce.getFiller().accept(this);
+    }
+
+    @Override
+    public Boolean visit(OWLObjectMinCardinality ce) {
+      return false;
+    }
+
+    @Override
+    public Boolean visit(OWLObjectExactCardinality ce) {
+      return false;
+    }
+
+    @Override
+    public Boolean visit(OWLObjectMaxCardinality ce) {
+      return false;
+    }
+
+    @Override
+    public Boolean visit(OWLObjectHasValue ce) {
+        return false;
+    }
+
+    @Override
+    public Boolean visit(OWLObjectHasSelf ce) {
+        return false;
+    }
+
+    @Override
+    public Boolean visit(OWLDataSomeValuesFrom ce) {
+        return false;
+    }
+
+    @Override
+    public Boolean visit(OWLDataAllValuesFrom ce) {
+        return false;
+    }
+
+    @Override
+    public Boolean visit(OWLDataHasValue ce) {
+        return false;
+    }
+
+    @Override
+    public Boolean visit(OWLDataMinCardinality ce) {
+        return false;
+    }
+
+    @Override
+    public Boolean visit(OWLDataExactCardinality ce) {
+        return false;
+    }
+
+    @Override
+    public Boolean visit(OWLDataMaxCardinality ce) {
+        return false;
+    }
+}
--- src/main/java/edu/vuamsterdam/MinimalConcepts/Main.java
+++ src/main/java/edu/vuamsterdam/MinimalConcepts/Main.java
@@ -4,13 +4,17 @@ import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.*;
 
 import java.io.File;
-import java.util.Optional;
+import java.util.concurrent.TimeoutException;
 import java.util.stream.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws OWLOntologyCreationException {
+    public static void main(String[] args) throws Exception {
+        processOntology(args[0]);
+    }
+    public static void processOntology(String filepath) throws Exception {
         OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
-        OWLOntology ontology = manager.loadOntologyFromOntologyDocument(new File("pizza-ontology/pizza.owl"));
+        OWLOntology ontology = manager.loadOntologyFromOntologyDocument(new File(filepath));
 
         Stream<OWLClassExpression> subAxiomExpressions = ontology.axioms()
                 .filter(OWLSubClassOfAxiom.class::isInstance)
@@ -20,20 +24,39 @@ public class Main {
         Stream<OWLClassExpression> eqAxiomExpressions = ontology.axioms()
                 .filter(OWLEquivalentClassesAxiom.class::isInstance)
                 .map(OWLEquivalentClassesAxiom.class::cast)
+                .filter(x -> x.classExpressions().noneMatch(OWLClassExpression::isClassExpressionLiteral))
                 .flatMap(OWLNaryClassAxiom::classExpressions);
 
-        Stream.concat(subAxiomExpressions, eqAxiomExpressions).forEach(expression -> minimizeExpression(expression, ontology));
-
+        try {
+            for (Iterator<OWLClassExpression> it = Stream.concat(subAxiomExpressions, eqAxiomExpressions).iterator(); it.hasNext(); ) {
+                OWLClassExpression expression = it.next();
+                try {
+                    minimizeExpression(expression, ontology);
+                } catch (TimeoutException e) {
+                    throw e;
+                } catch (Throwable t) {
+                    System.out.println("Could not Minimise: " + expression);
+                    if (expression.accept(new ClassExpressionFeasibleVisitor()) && !expression.accept(new ClassExpressionBuggedVisitor())) {
+                        System.out.println("WARNING: The unminimisable expression did not appear to be bugged");
+                    }
+                }
+            }
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+            System.out.println("Not minimizing " + filepath + "Took too long");
+        }
     }
-    private static void minimizeExpression(OWLClassExpression expression, OWLOntology ontology) {
+    private static void minimizeExpression(OWLClassExpression expression, OWLOntology ontology) throws Exception {
         System.out.println("---------------------------------------");
         System.out.println("Minimizing expression: " + expression);
         try {
             int origSize = expression.accept(new ClassExpressionSizeVisitor());
             System.out.println("Original size: " + origSize);
+            if (origSize == 1)
+                return;
 
             if (origSize < 5) {
-                SubsumptionLearningMinimalConcept minimalConceptGenerator = new SubsumptionLearningMinimalConcept(ontology, 0.02);
+                MinimalConcept minimalConceptGenerator = TimeoutHelper.runWithTimeout(() -> new TreeEquivilanceMinimalConcept(ontology), 60);
                 Optional<OWLClassExpression> newExpression = minimalConceptGenerator.getMinimalConcept(expression);
                 newExpression.ifPresent(expr -> System.out.println(
                         "Minimized expression to: " + expr + "\nWith size: " + expr.accept(new ClassExpressionSizeVisitor())
